b0c5d3032f007c8064335857bf77d4740a825b9b
Jenisha <jenishereyl@gmail.com>
2026-02-01 18:03:20 +0530

    Explore Flask basics with routes and templates

--- Model Deployment/flask/jinja.py
+++ Model Deployment/flask/jinja.py
@@ -7,7 +7,7 @@
 {#...#} this is for comments
 '''
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -23,12 +23,12 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/submit', methods=['GET', 'POST'])
-def submit():
-    if request.method == 'POST':
-        name =  request.form['name']
-        return f'Hello {name}!'
-    return render_template('form.html')
+# @app.route('/submit', methods=['GET', 'POST'])
+# def submit():
+#     if request.method == 'POST':
+#         name =  request.form['name']
+#         return f'Hello {name}!'
+#     return render_template('form.html')
 
 # variable rule
 @app.route('/sucess/<int:score>')
@@ -40,6 +40,42 @@ def sucess(score):
         res = 'FAILED'
 
     return render_template('result.html', results = res)
+
+@app.route('/sucessres/<int:score>')
+def sucessres(score):
+    res =''
+    if score >= 50:
+        res = "PASSED"
+    else:
+        res = 'FAILED'
+    
+    exp={'score':score, 'res':res}
+
+    return render_template('result1.html', results = exp)
+
+@app.route('/sucessif/<int:score>')
+def sucessif(score):
+    return render_template('result2.html', results = score)
+
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return render_template('result.html', results = score)
+
+
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    total_score=0
+    if request.method=='POST':
+        science=float(request.form['science'])
+        maths=float(request.form['maths'])
+        c=float(request.form['c'])
+        data_science=float(request.form['datascience'])
+
+        total_score=(science+maths+c+data_science)/4
+    else:
+        return render_template('getresult.html')
+    return redirect(url_for('sucessres',score=total_score))
 
 
 if __name__ == '__main__':
