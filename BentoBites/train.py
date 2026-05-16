21f047db0d41d3ae339da2c4d4b5ca351b7c2547
Jenisha <jenishereyl@gmail.com>
2026-05-16 18:45:00 +0530

    Added BentoML service and testing scripts

--- BentoBites/train.py
+++ BentoBites/train.py
@@ -14,3 +14,5 @@ clf.fit(X, y)
 # Save model to the BentoML local model store
 saved_model = bentoml.sklearn.save_model("iris_clf", clf)
 print(f"Model saved: {saved_model}")
+
+# iris_clf:i4i7phcsl2qejff3
